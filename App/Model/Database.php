e1ebb7ea8ca3f985c08b90627b4f01a0310825ec
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-01-29 21:56:53 -0300

    falta terminar o database

---
+++ App/Model/Database.php
@@ -0,0 +1,84 @@
+<?php
+
+class Database {
+    private $conn;
+    private string $local = "localhost";
+    private string $db = "clinicmedica";
+    private string $user = "root";
+    private string $password = "";
+    private string $table;
+    
+    function __construct($table = null){
+        $this->table = $table;
+        $this->conecta();
+    }
+
+    private function conecta(){
+        try {
+            $this->conn = new PDO("mysql:host=".$this->local.";dbname=".$this->db,$this->user,$this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+            echo "foi";
+        }
+        catch(PDOExption $err){
+            die("Connection failed ".$err->getMessage());
+        }
+    }
+
+    public function execute($query, $binds = []){
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute($binds);
+            return $stmt;
+        }
+        catch (PDOException $err){
+            die("Connection failed ".$err->getMessage());
+        }
+    }
+
+    public function insert($values){
+        $fields = array_keys($values);
+
+        $binds = array_pad([], count($fields), '?');
+
+        $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+
+        // echo '<pre>';
+        // print_r($query);
+        // echo '</pre>';
+        // exit;
+
+        $res = $this->execute($query, array_values($values));
+
+        if ($res){
+            return TRUE;
+        }
+        else {
+            return FALSE;
+        }
+    }
+}
+
+
+$db = new Database("medico");
+
+$medi = array(
+    'nome'=> 'clatlarinco',
+    'cpf' => '11111111111',
+    'crm' => '1111111111',
+    'especialidade' => 'urologista',
+    'telefone' => '11111111111',
+    'email' => 'rinco@gmail.com',
+    'senha' => 'pequeno123'
+);
+
+$res = $db->insert($medi);
+
+if ($res){
+    echo "cadastrado";
+}
+else {
+    echo 'nao foi cadastrado';
+}
+
+?>
