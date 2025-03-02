2169a620312c10f64cca3f546fc381e985ad7976
halysonreis <halysondasilvadosreis@gmail.com>
2025-03-02 11:09:38 -0400

    bulding menu

---
+++ App/View/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sistema da clinica medica</title>
+    <link rel="stylesheet" href="css/css-home.css">
+</head>
+<body>
+    <?php
+      include '../../Public/assets/menu.html';
+    ?>
+    <main class="main">
+       
+    </main>
+
+</body>
+</html>
