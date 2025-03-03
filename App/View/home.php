61e2f2afb84b3b23f321bf9cb344530ab8a820e1
halysonreis <halysondasilvadosreis@gmail.com>
2025-03-02 21:36:21 -0400

    create the menu home and create banner

--- App/View/home.php
+++ App/View/home.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>sistema da clinica medica</title>
-    <link rel="stylesheet" href="../../Public/css/css-menu.css">
+    <link rel="stylesheet" href="../../Public/css/css-home.css">
 </head>
 <body>
     <?php
-      include '../../Public/assets/menu.html';
+    include '../../Public/assets/menu.html';
+    include '../../Public/assets/banner.html'
     ?>
     <main class="main">
        
