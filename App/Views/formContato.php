7fc4c6bda39c19565a9d7a7e89226dc312cb456c
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-06-04 13:55:47 -0400

    .

---
+++ App/Views/formContato.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../../vendor/autoload.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contate-nos</h1>
+
+    <form action="" method="post">
+        <label>Nome</label>
+        <input type="text" name="nome" id="nome" require>
+        <br>
+        <label>E-mail</label>
+        <input type="email" name="email" id="email" require>
+        <br>
+        <label>Mensagem</label>
+        <br>
+        <textarea name="mensagem" id="mensagem" cols="30" rows="10" require></textarea>
+        <br>
+        <input type="reset" value="cancelar">
+        <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
